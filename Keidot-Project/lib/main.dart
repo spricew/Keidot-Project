7d0025f92850e7df7ca485b27205ca20a496fb5e
Heyder Medina <medinaheyder@gmail.com>
2025-02-06 18:40:32 -0600

    creacion de acc

--- Keidot-Project/lib/main.dart
+++ Keidot-Project/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:test_app/Services/transaction/service_transaction_controller.dart';
 import 'package:test_app/config/theme/app_theme.dart';
+import 'package:test_app/presentation/screens/account_screen.dart';
 import 'package:test_app/presentation/screens/home_page.dart';
 import 'package:test_app/presentation/screens/login_screen.dart';
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 2).theme(),
       home:
-          const Homepage(), // Página de inicio => Recuerrda cambiar esto por el Login despues de la pruebas de Stripe
+          const AccountScreen(), // Página de inicio => Recuerrda cambiar esto por el Login despues de la pruebas de Stripe
     );
   }
 }
--- Keidot-Project/lib/presentation/screens/account_screen.dart
+++ Keidot-Project/lib/presentation/screens/account_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
-class AccountScreenS extends StatelessWidget {
-  const AccountScreenS({super.key});
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: GridView.count(
+          crossAxisCount: 2, // Número de columnas
+          crossAxisSpacing: 10, // Espaciado horizontal
+          mainAxisSpacing: 10, // Espaciado vertical
+          padding: EdgeInsets.all(10),
+          children: List.generate(6, (index) {
+            return Container(
+              color: Colors.blueAccent,
+              child: Center(child: Text('Item $index', style: TextStyle(color: Colors.white))),
+            );
+          }),
+        ),
     );
   }
 }
