21de3a41d1c8d42c74918fe369bfa79f76309c37
Heyder Medina <medinaheyder@gmail.com>
2025-02-10 23:20:21 -0600

    añadido de pagina de solicitudes al menu

--- Keidot-Project/lib/widgets/custom_popup.dart
+++ Keidot-Project/lib/widgets/custom_popup.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:test_app/config/theme/app_theme.dart';
 import 'package:test_app/presentation/screens/client_profile_screen.dart';
 import 'package:test_app/presentation/screens/config_screen.dart';
+import 'package:test_app/presentation/screens/home_screen.dart';
+import 'package:test_app/presentation/screens/request_screen.dart';
+import 'package:test_app/presentation/screens/requests_screen.dart';
 
 class CustomPopupMenu extends StatelessWidget {
   const CustomPopupMenu({super.key});
@@ -28,7 +31,12 @@ class CustomPopupMenu extends StatelessWidget {
           ),
           PopupMenuItem(
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            onTap: () => _navigateToScreen(context, const ConfigScreen()),
+            onTap: () => _navigateToScreen(context, const RequestsScreen()),
+            child: _buildMenuText("Solicitudes"),
+          ),
+          PopupMenuItem(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            onTap: () => _navigateToScreen(context, const HomeScreen()),
             child: _buildMenuText("Acerca de"),
           ),
         ];
