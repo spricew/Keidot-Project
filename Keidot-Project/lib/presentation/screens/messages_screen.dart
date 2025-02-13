2bc272046d50bb2c386e2e1ed23e4c02cb2fad67
Heyder Medina <medinaheyder@gmail.com>
2025-02-13 11:37:20 -0600

    mensajes

--- Keidot-Project/lib/presentation/screens/messages_screen.dart
+++ Keidot-Project/lib/presentation/screens/messages_screen.dart
@@ -15,9 +15,47 @@ class MessagesScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         titleColor: darkGreen,
         iconColor: darkGreen,
-        onBackPressed: () {
-          // Volver al Home (índice 0)
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+        itemCount: 10, // Número de chats ficticios
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ListTile(
+              leading: const CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.white,
+                child: Icon(Icons.person, size: 35, color: darkGreen),
+              ),
+              title: Text(
+                'Contacto ${index + 1}',
+                style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: darkGreen),
+              ),
+              subtitle: const Text(
+                'Último mensaje recibido...',
+                style: TextStyle(color: Colors.grey, fontSize: 14),
+              ),
+              trailing: Text(
+                '12:30 PM',
+                style: TextStyle(color: Colors.grey[600]),
+              ),
+              onTap: () {
+                // Acción al tocar un chat
+              },
+            ),
+          );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Acción para iniciar una nueva conversación
+        },
+        backgroundColor: darkGreen,
+        child: const Icon(Icons.message, color: Colors.white),
       ),
     );
   }
