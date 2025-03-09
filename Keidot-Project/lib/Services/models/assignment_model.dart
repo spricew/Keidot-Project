e7f85fa4062db032890361735f5b9e932a0f7301
Diego Sanchez <eksanchezdiego24@example.com>
2025-03-08 22:33:53 -0600

    heyder is gay

--- Keidot-Project/lib/Services/models/assignment_model.dart
+++ Keidot-Project/lib/Services/models/assignment_model.dart
@@ -8,7 +8,7 @@ class AssignmentDTO {
   final DateTime timeSelected;
   final double amount;
   final String status;
-  // final DateTime createdAt; // Se agrega la fecha de creación
+  final DateTime createdAt; // Se agrega la fecha de creación
 
   AssignmentDTO({
     required this.idAssignment,
@@ -18,7 +18,7 @@ class AssignmentDTO {
     required this.timeSelected,
     required this.amount,
     required this.status,
-    // required this.createdAt,
+    required this.createdAt,
   });
 
   /// Formatea la fecha como `16/02/2025`
@@ -31,18 +31,28 @@ class AssignmentDTO {
     return DateFormat('hh:mm a').format(timeSelected);
   }
 
+  //Assignet_at formateada porque no lo hizo HeyderMomischis
+  /// Formatea la fecha como `16/02/2025`
+  String get formattedDatecreatedAt {
+    return DateFormat('dd/MM/yyyy').format(createdAt);
+  }
+
+  /// Formatea la hora seleccionada como `03:00 PM`
+  String get formattedTimecreatedAt {
+    return DateFormat('hh:mm a').format(createdAt);
+  }
+
   /// Convierte JSON a `AssignmentDTO`
   factory AssignmentDTO.fromJson(Map<String, dynamic> json) {
     return AssignmentDTO(
       idAssignment: json['id_assignment'] as String,
       nameOfService: json['name_of_service'] as String,
       description: json['description'] as String,
-      estimatedSize:
-          json['estimated_size'] as String, // Cambiado de Duration a String
+      estimatedSize:json['estimated_size'] as String, // Cambiado de Duration a String
       timeSelected: DateTime.parse(json['time_selected']),
       amount: (json['amount'] as num).toDouble(),
       status: json['status'] as String,
-      // createdAt: DateTime.parse(json['created_at']),
+      createdAt: DateTime.parse(json['created_at']),
     );
   }
 
@@ -55,7 +65,8 @@ class AssignmentDTO {
       'estimated_size': estimatedSize, // Ahora se envía como String
       'time_selected': timeSelected.toIso8601String(),
       'amount': amount,
-      'status': status
+      'status': status,
+      'assigned_at': assigne,
     };
   }
 }
