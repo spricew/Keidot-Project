a96c1ffb463ff5e7a1f66749af43536fef2945eb
Diego Sanchez <eksanchezdiego24@example.com>
2025-03-10 09:27:01 -0600

    fix(jobs_publish): change storage declaration to non-constant for proper initialization

--- Keidot-Project/lib/Services/worker_request/assignments_publish/jobs_publish.dart
+++ Keidot-Project/lib/Services/worker_request/assignments_publish/jobs_publish.dart
@@ -7,7 +7,7 @@ import 'package:test_app/Services/models/assignment_model.dart';
 class JobsPublishService {
   final Logger _logger = Logger();
   final String baseUrl = "https://keidot.azurewebsites.net/api/AssignmentByUser";
-  const FlutterSecureStorage storage = FlutterSecureStorage(); // Almacenamiento seguro
+  FlutterSecureStorage storage = FlutterSecureStorage(); // Almacenamiento seguro
 
   Future<List<AssignmentDTO>> fetchAllJobs() async {
     final url = Uri.parse("$baseUrl/WorkerServices");
