5b6a773bb1ecbb5da8f7d122e8fd70338c514174
Heyder Medina <medinaheyder@gmail.com>
2025-03-11 20:43:24 -0600

    feat(pending_work): add PendingWorkScreen and integrate with CustomPopupMenu

---
+++ Keidot-Project/lib/presentation/screens/pending_work_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PendingWorkScreen extends StatelessWidget {
+  const PendingWorkScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
